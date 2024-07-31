ee692c6320922c918860c893f25a4b3a68ce4a59
gorejkoff <neskazua63@gmail.com>
2024-07-31 12:47:01 +0300

    first commit

---
+++ src/js/app_js/app.js
@@ -0,0 +1,30 @@
+"use strict"
+
+const isMobile = {
+   Android: function () { return navigator.userAgent.match(/Android/i) },
+   BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i) },
+   iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i) },
+   Opera: function () { return navigator.userAgent.match(/Opera Mini/i) },
+   Windows: function () { return navigator.userAgent.match(/IEMobile/i) },
+   any: function () {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+   }
+};
+const isPC = !isMobile.any();
+if (isPC) { document.body.classList.add('_pc') } else { document.body.classList.add('_touch') };
+
+// медиазапрос
+const MAX1024 = window.matchMedia('(max-width: 1023.98px)');
+
+
+function throttle(callee, timeout) {
+   let timer = null;
+   return function perform(...args) {
+      if (timer) return;
+      timer = setTimeout(() => {
+         callee(...args);
+         clearTimeout(timer);
+         timer = null;
+      }, timeout)
+   }
+}
